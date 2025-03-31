863baa77903905b71b94c8b96a78234b4b620068
Akowuah Addo Baffour <Akowuah Addo Baffour>
2025-03-31 17:16:15 +0000

    Added some comments

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,8 @@
 def is_palindrome(string):
+    # The following lines checks to see if after reversing the string, it is still the same
     if string == string[::-1]:
         return True
     else:
         return False
+    # An example usage
 print(is_palindrome("racecar"))
